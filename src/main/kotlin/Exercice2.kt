bb3532a5ae3217e32fdbb3b84a7e06ac42761c4c
saad-jaghlal <saadjaghlal2000@gmail.com>
2023-10-23 21:12:19 +0100

    my-first

---
+++ src/main/kotlin/Exercice2.kt
@@ -0,0 +1,55 @@
+import java.util.Date
+enum class Difficulte{
+    Facile,Difficile
+}
+
+//class Cycliste
+class Cycliste(var nom:String,var prenom:String,var nationalite:String,var age:Int,var historique:MutableMap<Etape,Resultat>){
+    fun nouvelleEtape(etape :Etape, resultat :Resultat):Boolean{
+
+        if (!historique.containsKey(etape)){
+            historique.put(etape,resultat)
+            return true
+		}
+        return false
+	}
+    override fun toString(): String {
+        var his:String=""
+        for (historiques in historique){
+            his += "\n"+historiques
+        }
+        return "nom: ${nom} prenom: ${prenom} nationalite: ${nationalite} age :${age}  les Etape : ${his}"
+
+    }
+}
+//class Resultat
+// class Resultat(var tempe:Date,var classemant:Int){
+//
+//}
+sealed class  Resultat{
+    data class Termine(var tempsEffectue:Int,var classement:Int):Resultat()
+    class Abandonne(var distanceParcourue:Int, var raison:String):Resultat()
+}
+// class etape
+data class Etape(var nomE:String, val dateDebut:Date,var dateFin: Date,var distance:Double,var difficulte:Difficulte)//{
+//    override fun toString(): String {
+//        return "nom: ${nomE}  dateDebut: ${dateDebut} dateFin :${dateFin}  distance : ${distance}   difficulte : ${difficulte}"
+//    }
+//}
+fun main(){
+    val c1 =Cycliste("nore","Ahmed","Maroccan",25, mutableMapOf())
+    val e1 = Etape("Étape 1", Date(2023, 10, 1), Date(2023, 10, 5), 150.5, Difficulte.Facile)
+    val e2 = Etape("Étape 2",  Date(2023, 10, 8), Date(2023, 10, 12), 200.2, Difficulte.Difficile)
+    val e3 = Etape("Étape 3",  Date(2023, 10, 8), Date(2023, 10, 12), 180.0,  Difficulte.Facile)
+//    val resultat1 = Resultat(Date(2023, 10, 1, 10, 30, 5),5)
+//    val resultat2 = Resultat(Date(2023, 10, 8, 12, 15), 2)
+//    val resultat3 = Resultat(Date(2023, 10, 15, 9, 45), 3)
+    val resultat1 = Resultat.Termine(630, 5)
+    val resultat2 = Resultat.Termine(735, 2)
+    val resultat3 = Resultat.Abandonne(120, "problem")
+    c1.nouvelleEtape(e1, resultat1)
+    c1.nouvelleEtape(e2, resultat2)
+    c1.nouvelleEtape(e3, resultat3)
+    println(c1.toString())
+    println(c1)
+}
---
+++ src/main/kotlin/Exrcice1.kt
@@ -0,0 +1,95 @@
+data class joueur(var nom:String,var numero:Int,var equipe: EquipeP ,var nombreG:Int)
+//class joueur(var nom:String,var numero:Int,var equipe: EquipeP ,var nombreG:Int){
+//    fun getNom():String = nom
+//    fun getNum():Int = numero
+//}
+enum class EquipeP(var color:String ){
+    OCS("bleu"),OCK("rouge"),RCA("vert"), WAC("blanc"),
+}
+class Equipe(val p:Pays,val e:EquipeP,val joueurs: MutableList<joueur>){
+//    val joueurs = mutableListOf<joueur>()
+    //    this methode for recherche the player
+    fun recherche(j :joueur) :Int{
+//        methode 1
+//        for (i in 0 until joueurs.size){
+//            if (joueurs.get(i).numero == n){
+//                return i
+//            }
+//        }
+//        return -1
+        // methode 2
+        return joueurs.indexOfFirst { it.nom == j.nom }
+    }
+    //    this methode for ADD the player
+    fun ajout(j :joueur) :Boolean{
+        if (recherche(j)==-1){
+            joueurs.add(j)
+            return true
+        }
+        return false
+    }
+//    this methode for remove the player
+    fun suppression(j :joueur) : Boolean{
+        if(recherche(j)!=-1){
+//            remove by vzlue
+            joueurs.remove(j)
+//            remove by index
+//            joueurs.removeAt(recherche(j))
+            return true
+        }
+        return false
+    }
+    fun misaAjour(j:joueur):Boolean{
+        if(recherche(j)!= -1){
+            // methode recherche return index
+            joueurs[recherche(j)]=j
+            return true
+        }
+        return false
+
+    }
+    fun Afficher(){
+        // display the player information
+        for (joueur in joueurs){
+            println("nom: ${joueur.nom} numero:  ${joueur.numero} equipe: ${joueur.equipe} points: ${joueur.nombreG} Pays : ${p} ")
+        }
+    }
+    fun setPoints(j:joueur,points:Int):Boolean{
+        if (joueurs.indexOfFirst { it.nom == j.nom }!=-1){
+//            joueurs[recherche(j)].nombreG = points
+            joueurs[recherche(j)] = j.copy(nombreG = points)
+            return true
+        }
+        return false
+    }
+
+}
+enum class Pays{
+    MAROC, TUNISIE, EGYPTE, ALGERIE,MOURITANIE
+}
+fun main() {
+    val j1 = joueur("Ahmed",9,EquipeP.OCS,9)
+    val j2 =joueur("saad",5,EquipeP.OCK,15)
+    val j3 =joueur("yassine",8,EquipeP.OCS,8)
+    val j4 = joueur("Adil",10 ,EquipeP.OCK,5)
+    val E1 = Equipe(Pays.MAROC,EquipeP.OCS, mutableListOf())
+    val E2 = Equipe(Pays.ALGERIE,EquipeP.OCK, mutableListOf())
+    val E3 = Equipe(Pays.TUNISIE,EquipeP.WAC, mutableListOf())
+    val E4 = Equipe(Pays.EGYPTE,EquipeP.RCA, mutableListOf())
+     E1.ajout(j1)
+     E1.ajout(j3)
+    E2.ajout(j2)
+    E2.ajout(j4)
+    println("befor")
+    E1.Afficher()
+    E2.Afficher()
+    E1.suppression(j1)
+    E2.setPoints(j2,20)
+    E2.misaAjour(joueur("Adil",10 ,EquipeP.OCK,30))
+    println("After")
+    E1.Afficher()
+    E2.Afficher()
+
+
+
+}
